33e8f18211aaa77170277f8ce1786787ba52b68b
Lucas Eugênio <lucasseugenio@hotmail.com>
2020-11-21 13:33:18 -0300

    Change Assets Logic

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -10,3 +12,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# config/initializers/assets.rb
+if Rails.env.development?
+  Rails.application.config.assets.precompile += %w[graphiql/rails/application.js graphiql/rails/application.css]
+end
